126845330e708e22a9f76e7b565e918c1201906b
rohanganapavarapu <rohanzealous@gmail.com>
2021-04-12 16:02:54 -0700

    made it work for *nix and (attempted) to add response func

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,126 @@
+// #[macro_use]
+extern crate pyo3;
 
+use async_std::net::TcpStream;
+use async_std::task;
+use async_tls::client::TlsStream;
+use async_tls::TlsConnector;
+use futures::future;
+use futures::io::AsyncWriteExt;
+use pyo3::prelude::*;
+use std::net::{SocketAddr, ToSocketAddrs};
+
+#[pyclass]
+struct ConnectionManager {
+    address: SocketAddr,
+    streams: Vec<TcpStream>,
+}
+
+#[pymethods]
+impl ConnectionManager {
+    #[new]
+    fn new(host: String, port: u32) -> Self {
+        let addr_string = format!("{}:{}", host, port);
+        ConnectionManager {
+            address: addr_string.to_socket_addrs().unwrap().next().unwrap(),
+            streams: Vec::new(),
+        }
+    }
+
+    fn connect(&mut self, connections: u32) -> PyResult<()> {
+        async fn create_stream(address: SocketAddr) -> Option<TcpStream> {
+            let stream = TcpStream::connect(address).await;
+            stream.ok()
+        }
+
+        task::block_on(async {
+            self.streams.extend(
+                future::join_all((0..connections).map(|_| create_stream(self.address)))
+                    .await
+                    .into_iter()
+                    .filter_map(|s| s),
+            );
+        });
+
+        Ok(())
+    }
+
+    fn send(&mut self, payloads: Vec<&[u8]>) -> PyResult<()> {
+        let mut payloads = payloads.iter().cycle();
+        task::block_on(future::join_all(
+            self.streams
+                .iter_mut()
+                .map(|s| s.write_all(payloads.next().unwrap())),
+		stream.read_to_end(&mut resp).await?;
+		return resp;
+        ));
+
+        Ok(())
+    }
+}
+
+#[pyclass]
+struct TLSConnectionManager {
+    address: SocketAddr,
+    domain: String,
+    streams: Vec<TlsStream<TcpStream>>,
+}
+
+#[pymethods]
+impl TLSConnectionManager {
+    #[new]
+    fn new(host: String, port: u32, domain: String) -> Self {
+        let addr_string = format!("{}:{}", host, port);
+        TLSConnectionManager {
+            address: addr_string.to_socket_addrs().unwrap().next().unwrap(),
+            domain: domain,
+            streams: Vec::new(),
+        }
+    }
+
+    fn connect(&mut self, connections: u32) -> PyResult<()> {
+        let connector = TlsConnector::default();
+        async fn create_stream(
+            connector: &TlsConnector,
+            address: SocketAddr,
+            domain: &String,
+        ) -> Option<TlsStream<TcpStream>> {
+            let stream = TcpStream::connect(address).await.unwrap();
+            connector.connect(domain, stream).await.ok()
+        }
+
+        task::block_on(async {
+            self.streams.extend(
+                future::join_all(
+                    (0..connections).map(|_| create_stream(&connector, self.address, &self.domain)),
+                )
+                .await
+                .into_iter()
+                .filter_map(|s| s),
+            );
+        });
+
+        Ok(())
+    }
+
+    fn send(&mut self, payloads: Vec<&[u8]>) -> PyResult<()> {
+        let mut payloads = payloads.iter().cycle();
+	let mut resp = Vec::new();
+        task::block_on(future::join_all(
+            self.streams
+                .iter_mut()
+                .map(|s| s.write_all(payloads.next().unwrap())),
+		stream.read_to_end(&mut resp).await?;
+		reutrn resp;
+		));
+
+        Ok(())
+    }
+}
+
+#[pymodule]
+fn yet_another_http_client(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<ConnectionManager>()?;
+    m.add_class::<TLSConnectionManager>()?;
+    Ok(())
+}
